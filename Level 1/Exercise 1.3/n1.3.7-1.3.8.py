7dff5aab330d3ebc70d5fd4945696a6ecedc13bb
ComeOnGetMe <yzp451312410@gmail.com>
2017-10-07 22:06:46 -0700

    fix 1.3.7-1.3.8

--- Level 1/Exercise 1.3/n1.3.7-1.3.8.py
+++ Level 1/Exercise 1.3/n1.3.7-1.3.8.py
@@ -5,23 +5,24 @@ This program demonstrates the use of '**kwargs'
 
 # This is for 1.3.7
 def print_profile(name, age, **kwargs):
-    lists = [name, age] + [v for k, v in kwargs.items() if k in ['state', 'height', 'weight']]
-    for profile in zip(*lists):
-        print profile
+    print "name", name
+    print "age", age
+    if 'state' in kwargs:
+        print "state", kwargs['state']
+    if 'height' in kwargs:
+        print "height", kwargs['height']
+    if 'weight' in kwargs:
+        print "weight", kwargs['weight']
 
 
 # This is for 1.3.8
 def print_profile2(name, age, **kwargs):
-    lists = [name, age] + kwargs.values()
-    for profile in zip(*lists):
-        print profile
+    print "name", name
+    print "age", age
+    for k, v in kwargs.items():
+        print k, v
 
 
 if __name__ == '__main__':
-    name = ['Elle', 'Bob']
-    age = [18, 80]
-    state = ['NJ', 'NY']
-    height = [170, 180]
-    hobby = ['F1', 'Computer Game']
-    print_profile(name, age, state=state, height=height)
-    print_profile2(name, age, state=state, hobby=hobby)
+    print_profile('Elle', 18, state='married', height=170)
+    print_profile2('Bob', 20, state='single', hobby='Fishing')
